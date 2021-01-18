4935d6598c4d54942ba48eb39ce4d2349037d1a0
Renato Mello <renato.msf@gmail.com>
2021-01-18 05:00:52 -0300

    Select distinct tickers

--- db_backup.py
+++ db_backup.py
@@ -8,8 +8,19 @@ db_config = 'database.ini'
 #%%
 engine = psqlEngine(db_config)
 connection = engine.raw_connection()
+tickers_backup = read_sql_query("SELECT DISTINCT ticker FROM domestic_backup ORDER BY ticker", connection).ticker.to_list()
+# for ticker in tickers[-1:]:
+#     cursor = connection.cursor()
+#     cursor.execute("DELETE FROM domestic_backup WHERE ticker = '{}'".format(ticker))
+#     connection.commit()
+connection.close()
+engine.dispose()
+
+#%%
+engine = psqlEngine(db_config)
+connection = engine.raw_connection()
 tickers = read_sql_query("SELECT DISTINCT ticker FROM domestic ORDER BY ticker", connection).ticker.to_list()
-for k, ticker in enumerate(tickers):
+for k, ticker in enumerate(tickers[len(tickers_backup):]):
     print_string = '{}  {}'.format(len(tickers) - k - 1, ticker)
     stdout.write('\r\x1b[K' + print_string)
     stdout.flush()
